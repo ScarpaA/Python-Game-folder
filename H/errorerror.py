6b5681190dacace82d207a0699632c4a2bbd8453
Ayden Scarpa <113117810+ScarpaA@users.noreply.github.com>
2023-06-06 09:53:41 +0200

    Add files via upload

--- H/errorerror.py
+++ H/errorerror.py
@@ -1,21 +1,25 @@
-word="bonjour" # mot à chercher
-blankword="*******" # mot à deviner les étoiles seront remplacées au fur et à mesure
+word=" " # mot à chercher
+blankword=" " # mot à deviner les étoiles seront remplacées au fur et à mesure
 guletters=0 # compteur du nombre de lettres saisies par l'utilisateur
 
 def hangman(letter,word,blankword):
     newword=""
     error=0
     for i in range(len(word)):
-        if letter==word[i]:
-            blankword[i]==letter
-            newword=blankword
+        if word[i]==letter:
+            newword+=letter
         else:
-            error+=1
+            newword+=blankword[i]
     return newword
 
-while guletters<10 and blankword.count('*')!=0:
+while guletters<15 and blankword.count('*')!=0:
     letter=input("your letter")
     guletters+=1
     blankword=hangman(letter,word,blankword)
     print(blankword)
-    print(error)
+    print(guletters)
+
+if blankword.count('*')==0:
+    print("congrats, lowlife. You must feel real proud of yourself")
+else:
+    print("random guesses can only get you so far")
